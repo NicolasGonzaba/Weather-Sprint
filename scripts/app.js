500035183255bf3d02e6cb6000705c7cdb4e7bdb
NicolasGonzaba <nicolasgonzaba1@gmail.com>
2025-12-18 16:02:46 -0800

    Day 4

--- scripts/app.js
+++ scripts/app.js
@@ -12,14 +12,17 @@ let lowTemp4 = 10000000
 let highTemp4 = -10000000
 let lowTemp5 = 10000000
 let highTemp5 = -10000000
+
+let latitude
+let longitude = 0.1
+let longTime
+let timezone = -480
+
 // Geolocation script (DELETE LATER)-----------------------------
 const button = document.getElementById("getLocationBtn");
 const output = document.getElementById("output");
 const placeholder = document.getElementById("placeholder");
-let latitude
-let longitude = 0.1
-let time = " AM"
-let timezone = -480
+
 
 button.addEventListener("click", () => {
     // Check if the browser supports geolocation
@@ -40,6 +43,15 @@ button.addEventListener("click", () => {
             output.textContent = `Latitude: ${latitude}, Longitude: ${longitude}`;
             placeholder.innerText = "We got your location, check the console log to see an API.call for your weather"
 
+            longTime = longitude
+            if (-120 > longTime) {
+                if (longTime > -125) {
+                    longTime = -119
+                }
+
+            }
+            timezone = (Math.floor(longTime / 15)) * 60
+            console.log(timezone)
             fetchAPI()
             fetchForecast()
 
@@ -109,20 +121,40 @@ function fetchAPI() {
 
 
 }
+// --------------------------Date-------------------
 const currentDate = new Date()
 const dateOptions = { year: 'numeric', month: 'long', day: 'numeric' };
 const dayName = { weekday: 'long' };
+const dayShort = { weekday: 'short' }
+const forecast1 = new Date();
+const forecast2 = new Date();
+const forecast3 = new Date();
+const forecast4 = new Date();
+const forecast5 = new Date();
+
 console.log(currentDate.toLocaleDateString('en-US', dateOptions));
 currentDay.innerText = currentDate.toLocaleDateString('en-US', dateOptions)
 console.log(currentDate.toLocaleDateString('en-US', dayName));
 weekDay.innerText = currentDate.toLocaleDateString('en-US', dayName) + " (Today)"
+
+forecast1.setDate(currentDate.getDate() + 1)
+forecastDate1.innerText = forecast1.toLocaleDateString('en-US', dayShort)
+forecast1.setDate(currentDate.getDate() + 2)
+forecastDate2.innerText = forecast1.toLocaleDateString('en-US', dayShort)
+forecast1.setDate(currentDate.getDate() + 3)
+forecastDate3.innerText = forecast1.toLocaleDateString('en-US', dayShort)
+forecast1.setDate(currentDate.getDate() + 4)
+forecastDate4.innerText = forecast1.toLocaleDateString('en-US', dayShort)
+forecast1.setDate(currentDate.getDate() + 5)
+forecastDate5.innerText = forecast1.toLocaleDateString('en-US', dayShort)
+// ------------------------END of Date----------------
 
 // -----------------City Search----------------
 searchInput.addEventListener("keypress", function (event) {
     if (event.key === "Enter") {
         city = searchInput.value;
         findLocation()
-        searchInput.value = ""
+        // searchInput.value = ""
     }
 })
 
@@ -130,6 +162,7 @@ searchInput.addEventListener("keypress", function (event) {
 
 
 function findLocation() {
+    city = searchInput.value;
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${API_KEY}`) // Replace with your API endpoint
         .then(response => {
             if (!response.ok) {
@@ -143,11 +176,19 @@ function findLocation() {
             latitude = data[0].lat
             longitude = data[0].lon
 
-            timezone = (Math.floor(longitude / 15)) * 60
+            longTime = longitude
+            if (-120 > longTime) {
+                if (longTime > -125) {
+                    longTime = -119
+                }
+
+            }
+            timezone = (Math.floor(longTime / 15)) * 60
             console.log(timezone)
 
             fetchAPI()
             fetchForecast()
+            searchInput.value=""
         })
 
 
@@ -223,5 +264,122 @@ function fetchForecast() {
             }
             highTempFore5.innerText = highTemp5 + "°F"
             lowTempFore5.innerText = lowTemp5 + "°F"
+
+
+            forecastHumidity1.innerText = data.list[4].main.humidity + "%"
+            forecastHumidity2.innerText = data.list[12].main.humidity + "%"
+            forecastHumidity3.innerText = data.list[20].main.humidity + "%"
+            forecastHumidity4.innerText = data.list[28].main.humidity + "%"
+            forecastHumidity5.innerText = data.list[36].main.humidity + "%"
         })
+}
+
+
+
+
+//-----------Local storage-------------------------------------------------------
+const addToStorageBtn = document.getElementById("addToStorageBtn")
+const getFromStorageBtn = document.getElementById("getFromStorageBtn")
+const storedValue = document.getElementById("storedValue")
+const favBtn = document.getElementById("favBtn")
+
+
+
+
+favBtn.addEventListener('click', () => {
+    let nameInput = currentCity.innerText
+    saveToStorage(nameInput)
+    displayFavs()
+})
+
+
+
+
+const displayFavs = () => {
+    let favoriteList = getLocalStorage()
+    storedValue.innerHTML = ""
+    console.log(favoriteList)
+
+    favoriteList.forEach(city => {
+        console.log(city)
+
+        let p = document.createElement('p')
+        p.className = "m-2"
+
+        p.textContent = city
+
+        const deleteBtn = document.createElement('button')
+        deleteBtn.type = 'button'
+        deleteBtn.className = 'btn btn-danger mx-2'
+        deleteBtn.textContent = "Delete from Favs"
+
+        deleteBtn.addEventListener('click', () => {
+            removeFromStorage(city)
+            p.remove() //remove the Paragraph tag fromn the DOM
+        })
+        p.appendChild(deleteBtn)
+
+        const goBtn = document.createElement('button')
+        goBtn.type = 'button'
+        goBtn.className = 'btn btn-warning mx-2'
+        goBtn.textContent = "Go to City"
+
+        goBtn.addEventListener('click', () => {
+            searchInput.value = city
+            findLocation()
+        })
+        p.appendChild(goBtn)
+
+
+        storedValue.appendChild(p)
+    })
+}
+
+
+
+const saveToStorage = (city) => {
+    //creating an array of value to store into local storage
+    let cityArr = getLocalStorage();
+
+
+    //add a new name into our array
+    if (!cityArr.includes(city)) {
+        cityArr.push(city);
+    }
+
+    localStorage.setItem('Cities', JSON.stringify(cityArr))
+
+}
+
+//---------------------local Storage js-------------------
+//Retriving data from our local storage
+
+const getLocalStorage = () => {
+    //the data we're is an array
+
+    let value = localStorage.getItem('Cities')
+
+    //we check if null because we will be using a forEach and if we don't return the array our code will break
+    if (value === null) {
+        return [];
+    }
+    //JSON.parse coverts the string back into an array (or Original Form)
+    return JSON.parse(value)
+}
+
+
+//removing data from our local srorage *without the removeItem method
+
+const removeFromStorage = (city) => {
+    //name=Jesus
+    // nameIndex=1
+
+    let cityArr = getLocalStorage()
+
+    //find the index of the name in local storage
+    let cityIndex = cityArr.indexOf(city)
+
+    cityArr.splice(cityIndex, 1)
+
+    localStorage.setItem('Cities', JSON.stringify(cityArr))
 }
